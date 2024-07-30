93e69f53d4f3707af16b781185c46fe5cc056343
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-07-30 17:35:41 +0900

    Add Trie

--- app/src/main/java/code/editor/Syntax.java
+++ app/src/main/java/code/editor/Syntax.java
@@ -1,4 +1,49 @@
 package code.editor;
 
 public interface Syntax {
+
+    interface Lexer {
+
+    }
+
+
+
+    class LexerSource {
+        private CharSequence text;
+        private int position = -1;
+        private int peekCount = 0;
+        public LexerSource(CharSequence text) {
+            this.text = text;
+        }
+        public char readChar() {
+            position++;
+            peekCount = 0;
+            if (position >= text.length()) {
+                position = text.length();
+                return 0;
+            }
+            return text.charAt(position);
+        }
+        public char currentChar() {
+            return (position < 0 || position >= text.length()) ? 0 : text.charAt(position);
+        }
+        public char peekChar() {
+            peekCount++;
+            int index = position + peekCount;
+            if (index >= text.length()) {
+                peekCount = Math.min(text.length() - position, peekCount);
+                return 0;
+            }
+            return text.charAt(index);
+        }
+        public void commitPeek() {
+            position += peekCount;
+            peekCount = 0;
+        }
+        public void commitPeekBefore() {
+            position += Math.max(peekCount - 1, 0);
+            peekCount = 0;
+        }
+    }
+
 }
