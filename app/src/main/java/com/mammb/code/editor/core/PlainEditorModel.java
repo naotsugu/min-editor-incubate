64bb560745d0204c58522a21337b6bc9a4d6a7db
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-09-02 19:06:35 +0900

    Add FxActions

--- app/src/main/java/com/mammb/code/editor/core/PlainEditorModel.java
+++ app/src/main/java/com/mammb/code/editor/core/PlainEditorModel.java
@@ -21,8 +21,8 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class PlainEditorModel implements EditorModel {
+    protected double width = 0, height = 0;
     private final Content content;
-    private final TextLayout layout;
     private final ScreenBuffer<TextRow> buffer = ScreenBuffer.of();
     private final CaretGroup carets = CaretGroup.of();
 
@@ -32,23 +32,21 @@ public class PlainEditorModel implements EditorModel {
 
     public PlainEditorModel(Path path, FontMetrics fm) {
         this.content = Objects.isNull(path) ? Content.of() : Content.of(path);
-        this.layout = TextLayout.of(fm);
     }
 
     @Override
     public void draw(Draw draw) {
         draw.clear();
         carets.marked().forEach(range -> {
-            var l1 = layout.loc(range.start().row(), range.start().col());
-            var l2 = layout.loc(range.end().row(), range.end().col());
-            draw.fillRange(l1.x(), l1.y(), l2.x(), l2.y(),
-                    layout.marginLeft(), layout.width() - layout.marginLeft());
+
         });
     }
 
     @Override
     public void setSize(double width, double height) {
-        layout.setSize(width, height);
+        if (width < 0 || height < 0) return;
+        this.width = width;
+        this.height = height;
     }
 
     @Override
