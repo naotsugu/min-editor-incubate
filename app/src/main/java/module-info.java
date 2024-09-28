4e98becc9f0c03f50a25fcfb82db6490aa99afe9
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-09-29 00:18:35 +0900

    Modify layout

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -22,9 +22,6 @@ module code.editor {
     requires javafx.graphics;
     requires javafx.controls;
     requires com.mammb.code.piecetable;
-    exports code.editor;
-    exports code.editor.syntax;
-
     exports com.mammb.code.editor;
     exports com.mammb.code.editor.fx;
 }
